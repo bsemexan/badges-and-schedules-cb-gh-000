f64e7a59ef7902c19790173c617d8ae47076cae8
Berry  Semexan <bsemexan@gmail.com>
2018-02-13 05:34:25 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,7 +8,7 @@ def batch_badge_creator(names)
 end
 
 def assign_rooms(room)
-  room.each_with_index {|rm, idx| "Hello #{room[rm]}! You'll be assigned to room #{room[idx]}!"}
+  room.each_with_index {|rm, idx| room << "Hello #{rm}! You'll be assigned to room #{idx + 1}!"}
 end
 
 def printer
